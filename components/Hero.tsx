c278f2802c444661b94892ac92ec6a163c656ff8
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-13 20:56:30 +0300

    update hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,41 +1,60 @@
 import Link from 'next/link';
 import React from 'react'
+
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 //components
 import BackgroundCircles from './BackgroundCircles';
 
-type Props = {}
+import { PageInfo } from '../typings'
+import {urlFor} from "../sanity"
 
-function Hero({}: Props) {
+type Props = {
+    name?: string;
+    image?: string;
+    role?: string;
+    src?: string;
+}
+
+const Hero: React.FC<Props> = ({ name, image, role}) => {
     const [text, count] = useTypewriter({
         words: [
-            "Hi, I'm Mert Yücesoy",
+            `Hi, I'm ${name}`,
             "<LoveCoding>",
+            "Enjoy"
         ],
         loop: true,
         delaySpeed: 200,
     });
 
   return (
-    <div className='font-sans h-screen w-full flex flex-col space-y-12 justify-center items-center text-center overflow-hidden '>
+    <div className='font-sans h-[calc(100vh-80px)] w-full flex flex-col space-y-4 justify-center items-center text-center overflow-hidden '>
+        {/* animated background circle */}
         <BackgroundCircles />
-        <img
-            className='relative rounded-full h-32 w-32 mx-auto object-cover'
-            src='https://pbs.twimg.com/profile_images/1416753775667200001/gH_wiKf1_400x400.jpg'
-            alt=''
-        />
-        <div className='z-20 flex flex-col justify-start item-center text-center gap-5 w-full '>
-            <h2 className='text-sm uppercase text-[#ffb8b8]/60 tracking-[12px] p-2'>
-                Web Developer
-            </h2>
 
-            <h1 className='h-[100px] text-5xl lg:text-6xl font-semibold scroll-px-10'>
+        {/* picture of myself */}
+        <div className='relative w-32 h-32 md:w-40 md:h-40 mx-auto'>
+            <img
+                className='w-full h-full object-cover rounded-full'
+                src={urlFor(image)}
+                alt=''
+            />
+        </div>
+
+        <div className='z-20 flex flex-col justify-start item-center text-center gap-4 w-full '>
+            {/* job description */}
+            <h2 className='text-sm uppercase text-[#ffb8b8]/60 tracking-[12px] p-2'>
+                {role}
+            </h2>
+            
+            {/* animated text */}
+            <h1 className='h-[100px] text-4xl lg:text-6xl font-semibold scroll-px-10'>
                 <span className="mr-3">{text}</span>
                 <Cursor cursorColor='#ff7675' />
             </h1>
 
-            <div className='flex flex-col md:flex-row justify-center items-center text-center gap-2 pt-7 md:gap-2'>
+            {/* selection section */}
+            <div className='flex flex-col md:flex-row justify-center items-center text-center gap-2'>
                 <Link href="#about">
                     <button className='heroButton'>About</button> 
                 </Link>
