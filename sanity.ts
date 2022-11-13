efe50577344fb7835f19cf3c0ab83667b5b0cbbe
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-13 21:26:35 +0300

    update sanity backend api version

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-    apiVersion: "2022-11-09",
+    apiVersion: "2022-11-13",
     useCdn: process.env.NODE_ENV === "production",
 }
 
