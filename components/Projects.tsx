7a081c853299b578548bfc2197cfc2af7fbfc4da
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-13 20:56:47 +0300

    update project component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import { motion } from "framer-motion";
+import { Project } from '../typings'
+import {urlFor} from "../sanity"
 
-type Props = {}
+type Props = {
+  projects: Project[];
+  imageUrl?: string;
+}
 
-function Project({}: Props) {
-  const projects = [1, 2, 3, 4, 5]
+const Project: React.FC<Props> = ({projects}) =>{
   return (
     <motion.div 
     initial = {{ opacity: 0 }}
@@ -13,41 +17,64 @@ function Project({}: Props) {
     transition = {{ duration: 0.6 }}
     className='h-screen relative justify-center items-center'
     >
-      <div className='absolute h-fit top-24 flex flex-col overflow-hidden text-left max-w-full z-0  mx-auto '>
+      <div className='absolute h-[calc(100vh-96px)] top-24 flex flex-col overflow-hidden text-left max-w-full z-0  mx-auto '>
+        {/* page title */}
         <h3 className='w-full flex justify-center items-center uppercase tracking-[20px] text-gray-100 text-2xl'>
           Projects
         </h3>
 
-        <div className='hideScrollChrome h-fit w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+        {/* projects section */}
+        <div className='h-fit w-full flex justify-start overflow-x-scroll overflow-y-hidden z-20 rounded-[12px] snap-x snap-mandatory scrollbar-thin scrollbar-thumb-white/75 scrollbar-thumb-rounded-full'>
           {projects.map((project, i) => (
-            <div key={i} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-3 items-center justify-start p-10 md:p-44'>
-              <motion.img 
-                initial={{
-                  y:-300,
-                  opacity: 0,
-                }}
-                transition={{
-                  duration: 1.2
-                }}
-                whileInView={{
-                  y: 0,
-                  opacity: 1,
-                }}
-                viewport={{ once: true }}
-                src="https://camo.githubusercontent.com/dd4b2422ed3bfc9da88c43d18550375c66f9584327dff7ecc19315ce50b96f07/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f66697265626173652f66697265626173652d69636f6e2e737667"
-                alt=""
-              />
+            <div key={i} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-3 items-center justify-start p-10 gap-8 '>
 
-              <div className='space-y-8 px-0 md:px-10 max-w-6xl'>
-                <h4 className='text-4xl font-semibold text-center'>
+              {/* mockup of project */}
+              <div className='w-[310px] h-[170px] md:w-[620px] md:h-[340px]'>
+                <motion.img 
+                  initial={{
+                    y:-180,
+                    opacity: 0,
+                  }}
+                  transition={{
+                    duration: 1.2
+                  }}
+                  whileInView={{
+                    y: 0,
+                    opacity: 1,
+                  }}
+                  viewport={{ once: true }}
+                  className="w-full h-full object-cover rounded-xl"
+                  src={urlFor(project.image.asset["_ref"]) as any}
+                  alt=""
+                />
+              </div>
+
+              {/* info of project */}
+              <div className='flex flex-col gap-3 space-y-4 px-0 md:px-10 w-[300px] md:w-full max-w-6xl'>
+                {/* number of project */}
+                <h4 className='text-xl md:text-4xl font-semibold text-center'>
                   <span>
-                    Case Study {i + 1} of { projects.length}: 
+                    {project.title}
                   </span>
                 </h4>
-
-                <p className='text-lg text-center md:text-left'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                
+                {/* description of project */}
+                <p className='text-sm md:text-lg text-justify px-4 md:px-24'>
+                  {project?.summary}
                 </p>
+                
+                {/* used technologies */}
+                <div className='flex flex-wrap justify-center items-center gap-4'>
+                  {project?.technologies.map((tech) => 
+                  <div className='flex flex-col w-8 h-8 md:w-10 md:h-10'>
+                    <img
+                      className="w-full h-full object-cover"
+                      src={urlFor(tech.image.asset["_ref"]) as any}
+                    >
+                    </img>
+                  </div>
+                  )}
+                </div>
               </div>
             </div>
           ))}
