b0fec0efaeb0a69248a6ef820b52a820b9528e17
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-13 20:57:30 +0300

    update skills component

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,39 +1,50 @@
 import React from 'react'
 
 import { motion } from "framer-motion";
+import {urlFor} from "../sanity"
 
-type Props = {
+type Props = React.PropsWithChildren & {
     directionLeft?: boolean;
+    imageUrl?: string;
+    rate?: number;
+    title?: string;
 }
 
-function Skill({ directionLeft }: Props) {
+function Skill({ directionLeft, imageUrl, title, rate}: Props) {
   return (
-    <div className='group relative flex cursor-pointer'>
-        <motion.img 
-            initial={{
-                x: directionLeft ? -200 : 200,
-                opacity: 0,
-            }}
-            transition={{
-                duration: 1
-            }}
-            whileInView={{
-                x: 0,
-                opacity: 1,
-            }}
-            className="rounded-full border border-gray-500 object-cover w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
-            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg"
+    <motion.div 
+    initial={{
+        x: directionLeft ? -100 : 100,
+        opacity: 0,
+    }}
+    transition={{
+        duration: 1
+    }}
+    whileInView={{
+        x: 0,
+        opacity: 1,
+    }}
+    className='group relative flex cursor-pointer '>
+        {/* image of the skill */}
+        <div className='p-4 md:p-8 rounded-full w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 bg-white border border-gray-500  transition duration-300 ease-in-out'>
+            <img 
+            className=" w-full h-full object-cover "
+            src={urlFor(imageUrl) as any}
             alt=" "
-        />
-
-        <div className='absolute rounded-full opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-20 w-20 md:w-28 md:h-28 xl:w-32 xl:h-32 z-0'>
-            <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100'>
-                    90%
-                </p>
+            />
+        </div>
+        {/* knowledge rate of the skill */}
+        <div className='absolute rounded-full opacity-0 group-hover:opacity-90 transition duration-300 ease-in-out group-hover:bg-white h-20 w-20 md:w-28 md:h-28 xl:w-32 xl:h-32 z-0'>
+            <div className='flex flex-col items-center justify-center h-full'>
+                <div className='text-xs md:text-lg font-thin text-black opacity-100'>
+                    {title}
+                </div>
+                <div className='text-3xl font-bold text-black opacity-100'>
+                    %{rate}
+                </div>
             </div>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,38 +1,35 @@
 import React from 'react'
-import Skill from './Skill'
 
 import { motion } from "framer-motion";
+import { Skill as SkillType } from '../typings';
+import Skill from './Skill';
 
+type Props = {
+    skills: SkillType[]
+}
 
-type Props = {}
-
-function Skills({}: Props) {
+ const Skills: React.FC<Props> = ({skills}) => {
   return (
     <motion.div 
     initial={{ opacity: 0 }}
     whileInView={{ opacity: 1 }}
     transition={{ duration: 0.6 }}
     className='h-screen relative flex justify-center items-center'>
-        <div className='flex flex-col top-24 absolute justify-start items-center gap-12 xl:gap-20 text-center md:text-left max-w-[2000px] h-fit xl:space-y-0 mx-auto'>
+        {/* skills section */}
+        <div className='flex flex-col top-24 absolute justify-start items-center gap-8 xl:gap-16 text-center md:text-left max-w-[2000px] h-[calc(100vh-96px)] xl:space-y-0 mx-auto'>
+            {/* title section */}
             <div className='w-full flex flex-col justify-start items-center gap-3'>
                 <h3 className='uppercase tracking-[20px] text-gray-100 text-2xl'>
                     Skills
                 </h3>
                 <h3 className='uppercase tracking-[3px] text-gray-300 text-xs'>
-                    Hover over a skill for currency profieciency
+                    Hover over a skill for currency proficiency
                 </h3>
             </div>
-
-            <div className='grid grid-cols-2 md:grid-cols-3 mx-auto gap-3 md:gap-7'>
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
-                <Skill />
+            {/* skills */}
+            <div 
+            className='grid grid-cols-3 md:grid-cols-4 mx-auto gap-3 md:gap-7'>
+                {skills.map((skill) => <Skill title={skill.title} rate={skill.progress} imageUrl={skill.image.asset["_ref"]}>{skill.title}</Skill> )}
             </div>
         </div>
     </motion.div>
