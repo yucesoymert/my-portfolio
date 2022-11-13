1cfaacb75c67cd355fb360e3ed2ceb53117aca4a
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-13 20:53:51 +0300

    update background circles  component

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -21,7 +21,7 @@ function BackgroundCircles({}: Props) {
         <div className='absolute border border-[#474787] rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
         <div className='absolute border border-[#474787] rounded-full opacity-40 h-[300px] w-[300px] mt-52' />
         <div className='absolute border border-[#474787] rounded-full opacity-40 h-[500px] w-[500px] mt-52' />
-        <div className='absolute border border-[#ffb8b8] rounded-full opacity-40 h-[700px] w-[700px] mt-52 animate-pulse' />
+        <div className='absolute border border-[#ffb8b8]/40 rounded-full opacity-40 h-[700px] w-[700px] mt-52 animate-pulse' />
         <div className='absolute border border-[#474787] rounded-full opacity-50 h-[900px] w-[900px] mt-52'/>
     </motion.div>
   )
