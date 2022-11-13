01124ad5f318fa88e190146ed63fdc28dc877bf5
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-13 22:44:27 +0300

    update sanity process env

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    projectId: process.env.SANITY_PROJECT_ID!,
     apiVersion: "2022-11-13",
     useCdn: process.env.NODE_ENV === "production",
 }
