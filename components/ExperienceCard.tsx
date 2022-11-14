547c9c8fa323a421faa6f713e484c914cf5c7182
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-11-14 04:13:25 +0300

    update experience card height

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -14,7 +14,7 @@ type Props = React.PropsWithChildren & {
 
 function ExperienceCard({ companyName, jobTitle, imageUrl, dateEnded, dateStarted, tasks }: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-2 md:space-y-3 flex-shrink-0 w-[300px] md:w-[350px] h-[600px] md:h-[520px] xl:w-[600px] snap-center bg-[#474787]/60 p-4 opacity-40 hover:opacity-100 cursor-pointer gap-2 transition-opacity duration-200 overflow-hidden'>
+    <article className='flex flex-col rounded-lg items-center space-y-2 md:space-y-3 flex-shrink-0 w-[300px] md:w-[350px] h-[480px] md:h-[520px] xl:w-[600px] snap-center bg-[#474787]/60 p-4 opacity-40 hover:opacity-100 cursor-pointer gap-2 transition-opacity duration-200 overflow-hidden'>
         {/* image of company */}
         <div className='w-36 h-10 md:w-[200px] px-4 md:h-[80px] shadow-lg'>
             <motion.img
