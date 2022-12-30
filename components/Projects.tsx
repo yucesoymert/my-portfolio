0fe208c6ea93ab0fd1fd018b97992cafe833d717
Mert Yücesoy <mert.yucesoy1@gmail.com>
2022-12-30 18:10:39 +0300

    update projects page and add projects link on title

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,9 @@ type Props = {
   imageUrl?: string;
 }
 
+
 const Project: React.FC<Props> = ({ projects }) =>{
+  console.log({projects})
   return (
     <motion.div 
     initial = {{ opacity: 0 }}
@@ -18,7 +20,7 @@ const Project: React.FC<Props> = ({ projects }) =>{
     transition = {{ duration: 0.6 }}
     className='h-screen relative justify-center items-center'
     >
-      <div className='absolute h-[calc(100vh-96px)] top-24 flex flex-col overflow-hidden text-left max-w-full z-0  mx-auto '>
+      <div className='absolute h-[calc(100vh-16px)] top-24 flex flex-col overflow-hidden text-left max-w-full z-0  mx-auto '>
         {/* page title */}
         <h3 className='w-full flex justify-center items-center uppercase tracking-[20px] text-gray-100 text-2xl'>
           Projects
@@ -27,10 +29,10 @@ const Project: React.FC<Props> = ({ projects }) =>{
         {/* projects section */}
         <div className='h-fit w-full flex justify-start overflow-x-scroll overflow-y-hidden z-20 rounded-[12px] snap-x snap-mandatory scrollbar-thin scrollbar-thumb-white/75 scrollbar-thumb-rounded-full'>
           {projects.map((project, index) => (
-            <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-3 items-center justify-start p-10 gap-8 md:gap-4'>
+            <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-3 items-center justify-start p-10 gap-8 md:gap-3'>
 
               {/* mockup of project */}
-              <div className='w-[310px] h-[170px] md:w-[620px] md:h-[340px]'>
+              <div className='w-[310px] h-[170px] md:w-[500px] md:h-[280px]'>
                 <motion.img 
                   initial={{
                     y:-180,
@@ -53,10 +55,8 @@ const Project: React.FC<Props> = ({ projects }) =>{
               {/* info of project */}
               <div className='flex flex-col gap-3 space-y-4 px-0 md:px-10 w-[300px] md:w-full max-w-6xl'>
                 {/* number of project */}
-                <h4 className='text-xl md:text-4xl font-semibold text-center'>
-                  <span>
-                    {project.title}
-                  </span>
+                <h4 className='text-lg md:text-3xl text-center font-semibold'>
+                  <a className='hover:bg-white/10 px-3 py-2 hover:rounded-lg' href={project.linkToBuild} target="_blank">{project.title}</a>
                 </h4>
                 
                 {/* description of project */}
